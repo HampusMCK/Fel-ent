3af043ab70135497eb618883f493afa4ae6cbabd
Hampus Eriksson <Hampus.Eriksson@elev.ga.ntig.se>
2022-10-26 14:28:07 +0200

    update

--- scrip.js
+++ scrip.js
@@ -1,9 +1,14 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const column = document.querySelector(".column")
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active")
     navMenu.classList.toggle("active")
+})
+
+column.addEventListener("click", () => {
+    column.classList.toggle("active")
 })
 
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
